99dec7c99c3284884f92b8326a22259961c862f8
Eduardo Haag <eduardof.haag@gmail.com>
2023-10-17 15:48:12 -0300

    initial config

---
+++ src/main/java/me/dio/santanderdevweek2023/domain/model/Features.java
@@ -0,0 +1,11 @@
+package me.dio.santanderdevweek2023.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Features extends BaseItem {
+
+}
---
+++ src/main/java/me/dio/santanderdevweek2023/domain/model/News.java
@@ -0,0 +1,11 @@
+package me.dio.santanderdevweek2023.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class News extends BaseItem {
+
+}
